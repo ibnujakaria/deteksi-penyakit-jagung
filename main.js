cc1f7afe985d915d6b00f8450be8ff074f1ec8dc
Ibnu Jakaria <ibnujakaria@outlook.com>
2018-07-08 17:24:14 +0700

    get Run Percentage (RP) feature from GLRLM matrix

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let jimpToPixels = require('./src/prepare/from-jimp-to-pixels.js')
 let shortRunEmphasis = require('./src/glrlm/short-run-emphasis.js')
 let longRunEmphasis = require('./src/glrlm/long-run-emphasis.js')
 let greyLevelNonUniformity = require('./src/glrlm/grey-level-non-uniformity.js')
+let runPercentage = require('./src/glrlm/run-percentage.js')
 
 Jimp.read('./images/resized-removeobjects.jpg').then(image => {
   let pixels = jimpToPixels.getGreyscale(image)
@@ -15,8 +16,10 @@ Jimp.read('./images/resized-removeobjects.jpg').then(image => {
   let sre = shortRunEmphasis.compute(glrlm)
   let lre = longRunEmphasis.compute(glrlm)
   let gln = greyLevelNonUniformity.compute(glrlm)
+  let rp = runPercentage.compute(glrlm)
 
   console.log(`SRE: ${sre}`)
   console.log(`LRE: ${lre}`)
   console.log(`GLN: ${gln}`)
+  console.log(`RP: ${rp}`)
 })
---
+++ src/glrlm/run-percentage.js
@@ -0,0 +1,22 @@
+exports.compute = (glrlm) => {
+  let matrix = glrlm.matrix
+  let rp = 0
+  let n = matrix.rows.length * matrix.columns.length
+  // do a looping for every row
+  matrix.rows.forEach(row => {
+    // do a looping for every column, start from 1
+    let length = 1
+
+    row.forEach(col => {
+      if (col === 0) {
+        col = 0.1
+      }
+
+      let colMultipliedByLength = (col * length++)
+
+      rp += n / colMultipliedByLength
+    })
+  })
+
+  return rp
+}
