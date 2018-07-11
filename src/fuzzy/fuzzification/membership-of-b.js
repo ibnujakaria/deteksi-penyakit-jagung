f3c6ec8029195973c146a95fa7a6a8a20edfff7c
Ibnu Jakaria <ibnujakaria@outlook.com>
2018-07-12 05:41:19 +0700

    membership of b

---
+++ src/fuzzy/fuzzification/membership-of-b.js
@@ -0,0 +1,46 @@
+let Triangular = require('../membership/triangular.js')
+
+let triangular = new Triangular({
+  min: -107.8617,
+  max: 94.4758,
+  triangulars: [
+    {
+      label: 'blue',
+      a: -150,
+      b: -100,
+      c: -48
+    },
+    {
+      label: 'bluish',
+      a: -88,
+      b: -55,
+      c: -0
+    },
+    {
+      label: 'bmiddle',
+      a: -10,
+      b: 0,
+      c: 10
+    },
+    {
+      label: 'yellowish',
+      a: 0,
+      b: 48,
+      c: 78
+    },
+    {
+      label: 'yellow',
+      a: 38,
+      b: 94.4758,
+      c: 130
+    }
+  ]
+})
+
+console.log(triangular.membership(-50))
+console.log(triangular.membership(-40))
+console.log(triangular.membership(0))
+console.log(triangular.membership(0.4))
+console.log(triangular.membership(20))
+console.log(triangular.membership(40))
+console.log(triangular.membership(60))
