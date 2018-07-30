1fdcbf86a52c329cfc83d6d883a2b6a986d073b5
Ibnu Jakaria <ibnujakaria@outlook.com>
2018-07-30 14:14:49 +0700

    reconstruct k-fold generation

--- src/app/generate-training-and-testing-data.js
+++ src/app/generate-training-and-testing-data.js
@@ -10,72 +10,75 @@ let beautify = require("json-beautify")
 let fs = require('fs-extra')
 let rootPath = './images/penyakit-jagung'
 
-let data = []
-let imagesLength = 0
-let labels = fs.readdirSync(rootPath)
-
-labels.forEach(label => {
-  let _images = fs.readdirSync(`${rootPath}/${label}`)
-  let images = []
-
-  _images.forEach((path, no) => {
-    images.push({ no, path: `${label}/${path}`, label })
-    imagesLength++
-  })
-
-  data.push({ label, images })
-})
-
-fs.writeFileSync('./dist/labeled-data.json', beautify({ imagesLength, data }, null, 2, 100))
 
 // k-fold cross validation
-let k = 2
+let kMax = 10
 
-if (process.argv[2]) {
-  k = parseInt(process.argv[2])
-}
-
-let slicedData = []
-
-data.forEach(row => {
-  let lengthPerPartial = Math.floor(row.images.length / k)
-  let partials = []
-
-  for (let i = 0; i < k; i++) {
-    partials.push(row.images.splice(0, lengthPerPartial))
-  }
-
-  // if there is  anything left
-  if (row.images.length) {
-    partials[partials.length - 1] = partials[partials.length - 1].concat(row.images)
-  }
-
-  slicedData.push(partials)
-})
-
-fs.writeFileSync('./dist/labeled-data-sliced.json', beautify(slicedData, null, 2, 100))
-console.log('data output -> /dist/labeled-data-sliced.json')
-
-// generate training and testing data based on the k
 fs.removeSync('./dist/k-fold')
 fs.mkdirSync('./dist/k-fold')
-for (let i = 0; i < k; i++) {
-  let training = []
-  let testing = []
 
-  slicedData.forEach(data => {
-    data.forEach((images, j) => {
-      images.forEach(image => {
-        if (i === j) {
-          testing.push(image)
-        } else {
-          training.push(image)
-        }
-      })
+if (process.argv[2]) {
+  kMax = parseInt(process.argv[2])
+}
 
+for (let k = 2; k <= kMax; k++) {
+  let data = []
+  let imagesLength = 0
+  let labels = fs.readdirSync(rootPath)
+
+  labels.forEach(label => {
+    let _images = fs.readdirSync(`${rootPath}/${label}`)
+    let images = []
+
+    _images.forEach((path, no) => {
+      images.push({ no, path: `${label}/${path}`, label })
+      imagesLength++
     })
+
+    data.push({ label, images })
   })
 
-  fs.writeFileSync(`./dist/k-fold/data-${i + 1}.json`, beautify({ training, testing }, null, 2, 100))
-  console.log(`data output -> /dist/k-fold/data-${i + 1}.json`)
+  fs.writeFileSync('./dist/labeled-data.json', beautify({ imagesLength, data }, null, 2, 100))
+  
+  let slicedData = []
+
+  data.forEach(row => {
+    let lengthPerPartial = Math.floor(row.images.length / k)
+    let partials = []
+
+    for (let i = 0; i < k; i++) {
+      partials.push(row.images.splice(0, lengthPerPartial))
+    }
+
+    // if there is  anything left
+    if (row.images.length) {
+      partials[partials.length - 1] = partials[partials.length - 1].concat(row.images)
+    }
+
+    slicedData.push(partials)
+  })
+
+  fs.mkdirSync(`./dist/k-fold/k-${k}`)
+
+  // generate training and testing data based on the k
+  for (let i = 0; i < k; i++) {
+    let training = []
+    let testing = []
+
+    slicedData.forEach(data => {
+      data.forEach((images, j) => {
+        images.forEach(image => {
+          if (i === j) {
+            testing.push(image)
+          } else {
+            training.push(image)
+          }
+        })
+
+      })
+    })
+
+    fs.writeFileSync(`./dist/k-fold/k-${k}/data-${i + 1}.json`, beautify({ training, testing }, null, 2, 100))
+    console.log(`data output -> /dist/k-fold/k-${k}/data-${i + 1}.json`)
+  }
 }
