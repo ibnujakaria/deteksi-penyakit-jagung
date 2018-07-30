3c67e3fb295c6247f52f4de98558cf8d9c5b431c
Ibnu Jakaria <ibnujakaria@outlook.com>
2018-07-30 21:31:28 +0700

    change top bar using b-dropdown-item

--- src/client/components/TopNavbar.vue
+++ src/client/components/TopNavbar.vue
@@ -9,22 +9,22 @@
           <nuxt-link to="/data" class="nav-link">Data</nuxt-link>
         </li>
 
-        <b-nav-item-dropdown text="K-Fold" right>
-          <nuxt-link class="dropdown-item" to="/data/k-fold/2">Data K-2</nuxt-link>
-          <nuxt-link class="dropdown-item" to="/data/k-fold/3">Data K-3</nuxt-link>
-          <nuxt-link class="dropdown-item" to="/data/k-fold/4">Data K-4</nuxt-link>
-          <nuxt-link class="dropdown-item" to="/data/k-fold/5">Data K-5</nuxt-link>
-          <nuxt-link class="dropdown-item" to="/data/k-fold/6">Data K-6</nuxt-link>
-          <nuxt-link class="dropdown-item" to="/data/k-fold/7">Data K-7</nuxt-link>
-          <nuxt-link class="dropdown-item" to="/data/k-fold/8">Data K-8</nuxt-link>
+        <b-nav-item-dropdown text="K-Fold" left>
+          <b-dropdown-item to="/data/k-fold/2">Data K-2</b-dropdown-item>
+          <b-dropdown-item to="/data/k-fold/3">Data K-3</b-dropdown-item>
+          <b-dropdown-item to="/data/k-fold/4">Data K-4</b-dropdown-item>
+          <b-dropdown-item to="/data/k-fold/5">Data K-5</b-dropdown-item>
+          <b-dropdown-item to="/data/k-fold/6">Data K-6</b-dropdown-item>
+          <b-dropdown-item to="/data/k-fold/7">Data K-7</b-dropdown-item>
+          <b-dropdown-item to="/data/k-fold/8">Data K-8</b-dropdown-item>
         </b-nav-item-dropdown>
 
         <b-nav-item href="#">Hasil</b-nav-item>
 
-        <b-nav-item-dropdown text="Testing" right>
-          <nuxt-link class="dropdown-item" to="/testing/ekstraksi-fitur">Ekstraksi Fitur</nuxt-link>
-          <nuxt-link class="dropdown-item" to="/testing/rgb-to-linguistic-color">RGB to Linguistic Color</nuxt-link>
-          <nuxt-link class="dropdown-item" to="">GLRLM</nuxt-link>
+        <b-nav-item-dropdown text="Testing" left>
+          <b-dropdown-item to="/testing/ekstraksi-fitur">Ekstraksi Fitur</b-dropdown-item>
+          <b-dropdown-item to="/testing/rgb-to-linguistic-color">RGB to Linguistic Color</b-dropdown-item>
+          <b-dropdown-item to="">GLRLM</b-dropdown-item>
         </b-nav-item-dropdown>
       </b-navbar-nav>
     </b-collapse>
